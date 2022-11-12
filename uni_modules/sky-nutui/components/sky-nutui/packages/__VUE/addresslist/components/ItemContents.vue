cd258b4679183d7e08fb443242bb776a68da7a10
jwaterwater <jwaterwater@qq.com>
2022-11-12 15:52:54 +0800

    addresslist组件bug修复

--- uni_modules/sky-nutui/components/sky-nutui/packages/__VUE/addresslist/components/ItemContents.vue
+++ uni_modules/sky-nutui/components/sky-nutui/packages/__VUE/addresslist/components/ItemContents.vue
@@ -26,7 +26,7 @@
 </template>
 <script lang="ts">
 import { toRefs, reactive, onMounted, ref, watch } from 'vue';
-import { createComponent } from '../../utils/create';
+import { createComponent } from '../../../utils/create';
 const { componentName, create } = createComponent('item-contents');
 const { translate } = createComponent('addresslist');
 
